273fd2a8b9aff65e437054995664ef48a5ce5543
ruhul <ra.01403288711@gmail.com>
2023-04-13 16:45:53 +0600

    username and telephone number validation

--- project/validation.js
+++ project/validation.js
@@ -3,3 +3,28 @@ var reg = /[a-z]?pqr/ig; // i means insensitive , g means global
 //var reg2 = new RegExp(parm1 , parm2); // parm1 = regular expression , param2 = flag like(i,g)
 
 var reg2 = new RegExp(reg,'i')
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone:/^[0-9]{11}$/,
+    username:/^[a-z]{5,12}$/,
+    
+}
+
+function validate(field,regex){
+    // console.log(regex.test(field.value));
+    if(regex.test(field.value)){
+        field.className = "valid"
+    }
+    else field.className = "Not Valid"
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup',(e)=>{
+        console.log(e.target.attributes.name.value)
+        validate(e.target,patterns[e.target.attributes.name.value])
+        
+    })
+})
+
