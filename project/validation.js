866002572ff9ed48dea73dbf4fd0c52c7aed363e
ruhul <ra.01403288711@gmail.com>
2023-04-13 16:10:13 +0600

    regular expression decleration in js

---
+++ project/validation.js
@@ -0,0 +1,5 @@
+var reg = /[a-z]?pqr/ig; // i means insensitive , g means global
+
+//var reg2 = new RegExp(parm1 , parm2); // parm1 = regular expression , param2 = flag like(i,g)
+
+var reg2 = new RegExp(reg,'i')
