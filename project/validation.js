6306e0230611935864d61f1ab951ff8660539bd5
ruhul <ra.01403288711@gmail.com>
2023-04-13 17:06:07 +0600

    validation with regular expression and error message done

--- project/validation.js
+++ project/validation.js
@@ -1,34 +1,30 @@
-var reg = /[a-z]?pqr/ig; // i means insensitive , g means global
-
-//var reg2 = new RegExp(parm1 , parm2); // parm1 = regular expression , param2 = flag like(i,g)
-
-var reg2 = new RegExp(reg,'i')
-
 const inputs = document.querySelectorAll('input');
 
+// regex patterns
 const patterns = {
-    telephone:/^[0-9]{11}$/,
-    username:/^[a-z]{5,12}$/,
-    password:/^[\w@-]${8,20}/,
-    slug:/^[a-z\d-!]${8-20}/,
-    
+        telephone: /^\d{11}$/,
+        username: /^[a-z\d]{5,12}$/i,
+        password: /^[\d\w@-]{8,20}$/i,
+        slug: /^[a-z\d-]{8,20}$/,
+        email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+        //             yourname @ domain   .  com          ( .uk )
+};
 
+// validation function
+function validate(field, regex){
 
-}
-
-function validate(field,regex){
-    // console.log(regex.test(field.value));
     if(regex.test(field.value)){
-        field.className = "valid"
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
     }
-    else field.className = "Not Valid"
+
 }
 
-inputs.forEach((input)=>{
-    input.addEventListener('keyup',(e)=>{
-        console.log(e.target.attributes.name.value)
-        validate(e.target,patterns[e.target.attributes.name.value])
-        
-    })
-})
-
+// attach keyup events to inputs
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+            // console.log(patterns[e.target.attributes.name.value]);
+            validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
